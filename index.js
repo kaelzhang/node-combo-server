0212da66aa64be970ffcd5ffe16691a65f0b5756
Kael <i@kael.me>
2015-12-18 17:03:46 +0800

    removes old test cases, bin, and lib

--- index.js
+++ index.js
@@ -1,104 +1,141 @@
 'use strict'
 
-var express = require('express');
-var md5 = require('md5');
-var http = require('http');
-var ct = require('./lib/concat');
-var queue = require('./lib/queue');
+module.exports = middleware
 
-handle.prototype = {};
+var concat = require('./lib/concat')
+var AsyncCache = require('async-cache')
+var router = require('neuron-router')
+var async = require('async')
+var fs = require('graceful-fs')
+var unique = require('array-unique')
+var mime = require('mime');
 
-var que = new queue();
 
-function handle(argument){
-  var req = argument.req;
-  var res = argument.res;
-  var options = argument.options || {};
-  var cache = argument.cache;
-  var url = req.url;
-  var concat_name = "concat";
-  var file_arr = getFiles(url,concat_name);
-  var load_files = 0;
-  var concat = {
-    "reqUrl" : url,
-    "root" : options.root,
-    "name" : "concat",
-    "path" : "",
-    "file" : ""
-  };
-  concat.path = concat.root + "/" + concat.name;
-  concat.file = concat.path + "/" + concat.reqUrl;
+var node_url = require('url')
 
-  var result = {
-    "content" : "",
-    "status" : 200,
-    "type" : ""
-  }
 
-  var cache_options = {
-    max: 50000000,
-    maxAge: 1000 * 60 * 60,
-    stale: true
-  }
+// @param {Object} options
+// - parser: {function(url)} should returns {
+//     paths: {Array.<path>}
+//     version: {String=} optional
+//   }
+// - base: {String}
+// - joiner: {function()} method to join file contents
+// - routers: {Array.<router>}
+function middleware (options) {
+  var ac = new AsyncCache({
+    max: 1000,
+    maxAge: 1000 * 60 * 60 * 12,
+    load: function (url, callback) {
+      var parser = options.path_parser || middleware.parse_path
+      var parsed = parser(url)
 
-  var cache_key = md5(concat.file);
+      async.map(parsed.paths, function (path, done) {
+        middleware.get_content(path, options, done)
 
-  if(cache.has(cache_key)){
-    var cache_content = cache.get(cache_key);
-    result.content = cache_content
-    setReponse(res,result);
-    return;
-  }else{
-    var que_key = getFiles(url,concat_name);
-    var ready_que_key = 'ready:' + que_key;
-    que.on(ready_que_key, function(result){
-      //que.delete(ready_que_key);
-      setReponse(res,result);
+      }, function (err, contents) {
+        if (err) {
+          return callback(err)
+        }
+
+        callback(null, contents)
+      });
+    }
+  })
+
+  function handler (req, res, next) {
+    var url = req.url
+
+    ac.get(url, function (err, contents) {
+      if (err) {
+        return res.status(404).end('Failed to read "' + err.pathname + '"')
+      }
+
+      var joiner = options.joiner || middleware.join_contents;
+      var content = joiner(contents);
+      var last = contents[contents.length - 1].
+      var content_type = mime.lookup(last.filename);
+
+      res.status(200)
+      res.set('Content-Type', content_type)
+      res.set('Content-Length', content.length)
+      res.send(content)
+      res.end()
     })
-    console.log(que.list);
-    if(!que.has(que_key)){
-      que.add(que_key);
-    }else{
-      return;
-    }
-    console.log("没有cache");
   }
 
-  ct(file_arr, concat, function(err, result){
-    if(err){
-      console.log(err);
-      return;
-    }
-    if(result.toCache !== undefined && result.toCache === true){
-      console.log("******************")
-      cache.set(cache_key, result.content);
-      que.emit(ready_que_key,result); 
-    }
-    console.log(result);
+  return handler;
+}
 
-    setReponse(res,result);
+
+middleware.get_content = function (pathname, options, callback) {
+  router.route(path, {
+    routers: options.routers
+  }, function (filename, fallback_url) {
+    if (!filename) {
+      return callback({
+        code: 'ROUTE_NOT_FOUND',
+        path: pathname
+      })
+    }
+
+    fs.readFile(filename, function (err, content) {
+      if (err) {
+        return callback({
+          code: 'ERR_READ_FILE',
+          file: filename,
+          path: pathname,
+          err: err
+        })
+      }
+
+      callback(null, {
+        pathname: pathname,
+        filename: filename,
+        content: content.toString()
+      })
+    })
   })
 }
 
-function setReponse(res,result){
-  //console.log(result);
-  res.setHeader(
-    "Content-Length", 
-    result.content.length
-  );
-  res.setHeader(
-    "Content-Type", 
-    result.type
-  );
-  res.send(result.content);
-}
-function getQueueName(url,concat_name){
-  return url.replace("/"+ concat_name +"/","");
-}
-function getFiles(url,concat_name){
-  url = url.replace("/"+ concat_name +"/","");
-  url = url.replace(/\~/g,"/");
-  return url.split(",");
+
+// /combo/mod/a.js,mod/b.js
+// -> {
+//   paths: ['/mod/a.js', '/mod/b.js'],
+//   version: ''
+// }
+middleware.parse_path = function (url, options) {
+  var parsed = node_url.parse(url, true)
+  var version = parsed.query.v || ''
+  var pathname = parsed.pathname
+
+  var index = pathname.indexOf(options.base)
+  if (~index) {
+    pathname = pathname.slice(index + options.base.length)
+  }
+
+  var paths = pathname
+    .split(',')
+    .map(function (path) {
+      return middleware.make_sure_leading_slash(path)
+    })
+
+  return {
+    paths: unique(paths),
+    version: version
+  }
 }
 
-module.exports = handle
+
+middleware.join_contents = function (contents) {
+  return contents.map(function (content) {
+    var pathname = content.pathname;
+    return '// ' + pathname + '\n' + content.content;
+
+  }).join('\n\n');
+}
+
+
+middleware.make_sure_leading_slash = function (path) {
+  return path.replace(/^\/*/, '/')
+}
